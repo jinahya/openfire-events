de4d44c62350fd268b97f84fac975f11a893d051
Jin Kwon <onacit@gmail.com>
2018-08-12 18:43:24 +0900

    Add EventPayloadTest#payloadInstance()

--- src/test/java/com/github/jinahya/openfire/EventPayloadTest.java
+++ src/test/java/com/github/jinahya/openfire/EventPayloadTest.java
@@ -1,5 +1,7 @@
 package com.github.jinahya.openfire;
 
+import java.lang.reflect.Constructor;
+
 import static java.util.Objects.requireNonNull;
 
 public abstract class EventPayloadTest<T extends EventPayload> {
@@ -8,6 +10,19 @@ public abstract class EventPayloadTest<T extends EventPayload> {
     public EventPayloadTest(final Class<T> payloadClass) {
         super();
         this.payloadClass = requireNonNull(payloadClass, "payloadClass is null");
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    protected T payloadInstance() {
+        try {
+            final Constructor<T> constructor = payloadClass.getDeclaredConstructor();
+            if (!constructor.isAccessible()) {
+                constructor.setAccessible(true);
+            }
+            return constructor.newInstance();
+        } catch (final ReflectiveOperationException roe) {
+            throw new RuntimeException(roe);
+        }
     }
 
     // -----------------------------------------------------------------------------------------------------------------
